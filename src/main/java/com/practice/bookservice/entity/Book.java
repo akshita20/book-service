b41ae9166c299cb2c0a5f4c073c41afa071d9663
Akshita_Trehan <akshitatrehan95@gmail.com>
2021-06-12 18:46:13 +0530

    Add Spring config client

--- src/main/java/com/practice/bookservice/entity/Book.java
+++ src/main/java/com/practice/bookservice/entity/Book.java
@@ -1,8 +1,10 @@
 package com.practice.bookservice.entity;
 
 import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
